47408b545292443b1a3f1a86b6e0e2c380de6c1a
royal-dargon <2312749625@qq.com>
2022-04-11 23:06:44 +0800

    add:try some different kinds about pyecharts

---
+++ docs/note.md
@@ -0,0 +1,45 @@
+#### 关于pyecharts的一些学习心得
+
+##### 条形图
+
+```python
+# 下面是对条形图进行的测试
+bar = Bar()
+bar.add_xaxis(["数统","计科","物院","文院"])
+bar.add_yaxis("学院",[5,8,2,7])
+
+# bar.render_notebook() 这是在notebook上面进行直接展示使用的
+# 下面一种是可以直接生产html文件的方法，值得注意的是其中也是可以添加路径参数的
+bar.render()
+```
+
+##### 散点图
+
+```python
+scatter = Scatter()
+scatter.add_xaxis(Faker.choose())
+scatter.add_yaxis("商家", Faker.values())
+
+# 这里是一些全局变量的设置，主要是标题和时候画出分隔线，也许其中会有很多不一样的尝试机会
+scatter.set_global_opts(
+    title_opts=opts.TitleOpts(title="显示分隔线"),
+    xaxis_opts=opts.AxisOpts(splitline_opts=opts.SplitLineOpts(is_show=True)),
+    yaxis_opts=opts.AxisOpts(splitline_opts=opts.SplitLineOpts(is_show=True)),
+)
+
+scatter.render()
+```
+
+##### 层叠多图
+
+```python
+def overlap_line_scatter():
+    x = Faker.choose()
+    bar = Bar().add_xaxis(x).add_yaxis("商家A", Faker.values()).add_yaxis("商家B", Faker.values())
+    line = Line().add_xaxis(x).add_yaxis("商家A", Faker.values()).add_yaxis("商家B", Faker.values())
+    bar.overlap(line)
+    return bar
+
+overlap_line_scatter().render()
+```
+
---
+++ pyecharts_test/practice.py
@@ -0,0 +1,43 @@
+# 这是我对各种图进行训练的文件
+from pyecharts.charts import Bar, Scatter, Line
+from pyecharts import options as opts
+from pyecharts.faker import Faker
+
+# 下面是对条形图进行的测试
+bar = Bar()
+bar.add_xaxis(["数统","计科","物院","文院"])
+bar.add_yaxis("学院",[5,8,2,7])
+
+# bar.render_notebook() 这是在notebook上面进行直接展示使用的
+# 下面一种是可以直接生产html文件的方法，值得注意的是其中也是可以添加路径参数的
+# bar.render()
+
+# 下面是生成散点图一种方式
+scatter = Scatter()
+scatter.add_xaxis(Faker.choose())
+scatter.add_yaxis("商家", Faker.values())
+
+# 这里是一些全局变量的设置，主要是标题和时候画出分隔线，也许其中会有很多不一样的尝试机会
+scatter.set_global_opts(
+    title_opts=opts.TitleOpts(title="显示分隔线"),
+    xaxis_opts=opts.AxisOpts(splitline_opts=opts.SplitLineOpts(is_show=True)),
+    yaxis_opts=opts.AxisOpts(splitline_opts=opts.SplitLineOpts(is_show=True)),
+)
+
+# scatter.render()
+
+# 下面开始尝试层叠多图
+def overlap_line_scatter():
+    x = Faker.choose()
+    bar = Bar().add_xaxis(x).add_yaxis("商家A", Faker.values()).add_yaxis("商家B", Faker.values())
+    line = Line().add_xaxis(x).add_yaxis("商家A", Faker.values()).add_yaxis("商家B", Faker.values())
+    bar.overlap(line)
+    return bar
+
+overlap_line_scatter().render()
+
+
+
+
+
+
