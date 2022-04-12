62518c1cb99be9689b47b978f9589994412eb06c
royal-dargon <2312749625@qq.com>
2022-04-12 14:57:53 +0800

    add:add some codes about csv

---
+++ pyecharts_test/practice_csv.py
@@ -0,0 +1,33 @@
+# 这是一个对csv形式进行解析的demo
+import pandas as pd
+from pyecharts.charts import Bar
+
+
+def csv_data_show(csv_file, x_head_key):
+    # 这个函数的目的是去读取csv内部的内容
+    df = pd.read_csv(csv_file, encoding='gbk')
+    # df = pd.DataFrame(df)
+    cols_len = len(df.columns)
+    # print(cols_len)   9
+    rows_len = len(df)
+    # print(rows_len)
+    # x_head = [str(c).strip() for c in df[x_head_key]]
+    # temp = set(x_head)
+    # print(temp)
+    res = df.groupby([x_head_key], as_index=False)['rate'].agg({'cnt': 'count'})
+    tag_list = res["rate"]
+    tag_name = tag_list[2:]
+    count_list = res['cnt']
+    count = count_list[2:]
+    bars_show(tag_name, count)
+
+
+def bars_show(x, y):
+    bar = Bar()
+    bar.add_xaxis(list(x))
+    bar.add_yaxis("风险等级", list(y))
+    bar.render()
+
+
+
+csv_data_show("result1.csv", 'rate')
